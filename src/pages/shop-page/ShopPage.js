8f16f7b587892ad7dcd1ca02816a3a091dbb0ef8
Inna Mon <inna.mon@gmail.com>
2020-03-18 19:01:41 -0500

    added collection preview comp displaying collection data

--- src/pages/shop-page/ShopPage.js
+++ src/pages/shop-page/ShopPage.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 import SHOP_DATA from './ShopData';
 
+import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
+
 class ShopPage extends Component {
     state = {
         collections: SHOP_DATA
     }
     render() {
+        const { collections } = this.state;
         return (
-            <div>SHOP PAGE</div>
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
         );
     }
 }
