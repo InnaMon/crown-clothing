b88c37cc5f1b49a8c8016e3bb48df58fd5e19582
Inna Mon <inna.mon@gmail.com>
2020-03-19 18:25:42 -0500

    added sign-in and form-input components with styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop-page/ShopPage';
+import SignInAndSignUp from './pages/sign-in-and-sign-up-page/SignInAndSignUp';
 import Header from './components/Header/Header';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopPage} />
+        <Route  path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
