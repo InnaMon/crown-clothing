c8ef2ddf956d48857c0013d8511a01eb44bc2e9e
Inna Mon <inna.mon@gmail.com>
2020-04-16 13:36:12 -0500

    added sign in intergration with sign in and sign out links and styles

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+//this imports the necesssary firebase products
 
 const config = {
     apiKey: "AIzaSyAvUklnqk_LiJUPyo0lrYtUQZ--Jkg3amg",
@@ -12,11 +13,14 @@ const config = {
     appId: "1:1033564853996:web:f84981102255b08395b7c9",
     measurementId: "G-DHE7HPHTMH"
   };
+  //provides Firebase project configuration specific to your app
 
   firebase.initializeApp(config);
+  //initializes app
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
+  //gives access to default project's Firebase services throught app
 
   // want to trigger Google pop up whenever we use this Google auth
   const provider = new firebase.auth.GoogleAuthProvider();
