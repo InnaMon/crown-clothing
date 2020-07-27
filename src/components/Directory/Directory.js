aa1ef35d48d3a39e817073a3bfebb313f9699ba1
Inna Mon <inna.mon@gmail.com>
2020-07-27 13:51:49 -0500

    render spinner until all directpry img urls are finished loading

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectDirectorySection } from '../../redux/directory/directory.selectors';
+import Spinner from '../WithSpinner/Spinner';
 
 import MenuItem from '../MenuItem/MenuItem';
 import './Directory.scss';
 
-const Directory = ({ sections }) => (
-  <div className='directory-menu'>
+const Directory = ({ sections }) => {
+  const [loading, setLoading] = useState(true);
+  const [counter, setCounter] = useState(0)  
+
+  const loadHandler =() => {
+    if (counter < sections.length) {
+      setCounter(prevState => prevState+1)
+    }
+  }
+
+  useEffect(() => {
+    const stopLoader = () => {
+      if (counter >= sections.length) {
+        setLoading(false);
+      }
+    }
+    stopLoader()
+  }, [counter, sections])
+
+  console.log('counter', counter)
+  console.log('sections.length', sections.length)
+
+ return (<>  
+  <div style={{display: loading ? "block" : "none"}}>
+    <Spinner />
+  </div> 
+
+  <div className='directory-menu' style={{visibility: loading ? "hidden" : "visible"}}>
     {
       sections.map(({id, ...otherSectionProps}) => (
           <MenuItem key={id}
+          loadHandler={loadHandler}
           {...otherSectionProps}/>
       ))
     }
   </div>
-)
+
+  </>
+  )
+}
 
 const mapStateToProps = createStructuredSelector({
   sections: selectDirectorySection
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -3,17 +3,18 @@ import { withRouter } from 'react-router-dom';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl, history, match}) => (
-    <div className={`${size} menu-item`}
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match, loadHandler}) => {
+
+     return (<div className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-      <div className='background-image'
-      style={{ backgroundImage: `url(${imageUrl})` }} />
+      <img className='background-image' src={imageUrl} alt="background" onLoad={loadHandler} />
       <div className='content'>
         <div className='title'>{title.toUpperCase()}</div>
         <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>
-);
+  );
+}
 
 export default withRouter(MenuItem);
